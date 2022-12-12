d36d3214f5f21d9841a279a9bfeae65bf11871c3
Noxyangel <forgotrrr@gmail.com>
2022-12-12 22:34:51 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { apiData, data } from './store';
+
+	let countryName: string = 'Germany';
+	$: link = 'https://restcountries.com/v3.1/name/' + countryName;
+	const fetchCountryData = async () => {
+		fetch(link)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				apiData.set(data);
+			})
+			.catch((error) => {
+				console.log(error);
+				return [];
+			});
+	};
+</script>
+
+<input bind:value={countryName} />
+<button on:click={fetchCountryData}>Search</button>
+<article><h1>{data}</h1></article>
