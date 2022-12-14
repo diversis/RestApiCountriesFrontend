e76e972210ac3fd00c6ff5c4175eabd48744e6b0
Noxyangel <forgotrrr@gmail.com>
2022-12-14 14:49:44 +0300

    refactor load page 0

--- src/routes/countries/[slug]/+page.svelte
+++ src/routes/countries/[slug]/+page.svelte
@@ -1,78 +1,89 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte/types/runtime/internal/lifecycle';
 	import { apiData } from './store';
 
-	export let data: PageData;
-	console.log('+page data: ', data);
+	$: countryStoreIndex = 0;
+	onMount(async () => {
+		fetch(`https://restcountries.com/v3.1/name/${$page.params.slug}`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				const newData = { name: $page.params.slug, countryData: data[0] };
+				let name = $apiData.find((entry) => entry.name === data.name);
+				countryStoreIndex = $apiData.indexOf(name);
+				if (!name) {
+					$apiData = [...$apiData, newData];
+				}
+				apiData.set(data);
+			})
+			.catch((error) => {
+				console.log(error);
+				return [];
+			});
+	});
+	console.log('\n--------------------\n+page apidata: ', $apiData, '\n--------------------\n');
+	console.log('\n--------------------\n+page apidata: ', $apiData, '\n--------------------\n');
 
-	$: flag = '' + data.countryData[0]?.name.common + ' flag';
-	const addCountryToStore = () => {
-		$apiData = [...$apiData, data.countryData[0]];
-	};
-	addCountryToStore();
-	console.log('+page apidata: ', $apiData);
+	$: flag = '' + $page.params.slug + ' flag';
+	$: country = $apiData[countryStoreIndex]?.countryData;
 </script>
 
 <article>
 	<div class="container grid">
-		<img
-			src={data.countryData[0]?.flags.png}
-			alt={'' + data.countryData[0]?.name.common + ' flag'}
-		/>
+		<img src={country.flags.png} alt={'' + country.name.common + ' flag'} />
 		<div>
-			<h1 id="country-name">{data.countryData[0]?.name.common}</h1>
+			<h1 id="country-name">{country.name.common}</h1>
 			<!-- Info -->
 			<div class="grid">
 				<!-- Column 1 -->
 				<div class="flex">
 					<p>
 						<span>Native Name: </span>
-						{$apiData[0]?.name?.nativeName[
-							Object.keys($apiData[0].name.nativeName)[
-								Object.keys($apiData[0].name.nativeName).length - 1
+						{$apiData[countryStoreIndex]?.name?.nativeName[
+							Object.keys($apiData[countryStoreIndex].name.nativeName)[
+								Object.keys($apiData[countryStoreIndex].name.nativeName).length - 1
 							]
 						].common}
 					</p>
 					<p>
 						<span>Population: </span>
-						{$apiData[0]?.population}
+						{$apiData[countryStoreIndex]?.population}
 					</p>
 					<p>
 						<span>Region: </span>
-						{$apiData[0]?.region}
+						{$apiData[countryStoreIndex]?.region}
 					</p>
 					<p>
 						<span>Sub Region: </span>
-						{$apiData[0]?.subregion}
+						{$apiData[countryStoreIndex]?.subregion}
 					</p>
 					<p>
 						<span>Capital: </span>
-						{$apiData[0]?.capital}
+						{country?.capital}
 					</p>
 				</div>
 				<!--  Column 2  -->
 				<div class="flex">
 					<p>
 						<span>Top Level Domain: </span>
-						{$apiData[0].tld}
+						{country?.tld}
 					</p>
 					<p>
 						<span>Currencies: </span>
-						{$apiData[0]?.name?.nativeName[
-							Object.keys($apiData[0].name.nativeName)[
-								Object.keys($apiData[0].name.nativeName).length - 1
-							]
+						{country?.name?.nativeName[
+							Object.keys(country.name.nativeName)[Object.keys(country.name.nativeName).length - 1]
 						].common}
 					</p>
 					<p>
 						<span>Languages: </span>
-						{#each Object.values($apiData[0]?.languages) as lang}
+						{#each Object.values(country?.languages) as lang}
 							{lang + ' '}
 						{/each}
 					</p>
 				</div>
 				<div class="flex">
-					{#each data.countryData[0]?.borders as border}
+					{#each country?.borders as border}
 						<a class="btn" href={'https://restcountries.com/v3.1/alpha/' + border}>{border}</a>
 					{/each}
 				</div>
--- src/routes/countries/[slug]/+page.ts
+++ src/routes/countries/[slug]/+page.ts
@@ -5,8 +5,10 @@ function match(param) {
 	return /^\d+$/.test(param);
 }
 
-export async function load({ fetch, params }: PageLoad) {
-	const res = await fetch(`https://restcountries.com/v3.1/name/${params.slug}`);
-	const countryData = await res.json();
-	return { countryData };
+export async function load({ params }: PageLoad) {
+	// const res = await fetch(`https://restcountries.com/v3.1/name/${params.slug}`);
+	// const data = await res.json();
+	// const name = params.slug;
+	// const countryData = { ...data[0] };
+	return { name: params.slug };
 }
