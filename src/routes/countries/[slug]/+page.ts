a006d464e6cf495b1e985ffd0b72f7cd796883b0
Noxyangel <forgotrrr@gmail.com>
2022-12-14 15:06:28 +0300

    2

--- src/routes/countries/[slug]/+page.ts
+++ src/routes/countries/[slug]/+page.ts
@@ -1,14 +1,6 @@
 import type { PageLoad } from './$types';
-import type { ParamMatcher } from '@sveltejs/kit';
-
-function match(param) {
-	return /^\d+$/.test(param);
-}
-
-export async function load({ params }: PageLoad) {
-	// const res = await fetch(`https://restcountries.com/v3.1/name/${params.slug}`);
-	// const data = await res.json();
-	// const name = params.slug;
-	// const countryData = { ...data[0] };
-	return { name: params.slug };
-}
+export const load = ({ params }) => {
+	return {
+		slug: params.slug
+	};
+};
