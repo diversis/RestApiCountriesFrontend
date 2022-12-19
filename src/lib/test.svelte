48049fedc635de3ee6bad8faddb19998ca69f813
Noxyangel <forgotrrr@gmail.com>
2022-12-19 22:51:45 +0300

    back to arrays 0

---
+++ src/lib/test.svelte
@@ -0,0 +1,13 @@
+<script>
+	import {onMount} from 'svelte'
+	import {data} from './store'
+	let name = 'world';
+	let x= ['rw','re','gr','fds']
+	onMount(() =>{
+		for (each w of x){
+		data.update(data =>{
+			data.w = 'gf'+w;
+			return data
+		})}
+	})
+</script>
