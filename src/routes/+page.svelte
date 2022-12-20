cf497b26cb2a24f18b7acc8e9cba39a7d651d20d
Noxyangel <forgotrrr@gmail.com>
2022-12-20 18:53:33 +0300

    preupdate 0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,31 @@
 <script>
 	import BackButton from '$lib/BackButton.svelte';
+	import CardSmall from '$lib/CardSmall.svelte';
 	import { searchCountires } from '$lib/Country';
 	import Header from '$lib/Header.svelte';
+	import LoaderCog from '$lib/Loader-cog.svelte';
 	import LoaderInline from '$lib/Loader-inline.svelte';
-	import '../index.css';
+	import '../../../index.css';
 
 	export let data;
 
 	$: slug = data?.slug;
+
 	$: searchString = '';
 </script>
 
 <Header />
 <main class="relative">
 	<article
-		class="container mx-auto flex flex-col lg:grid lg:grid-cols-2 items-center mt-12 px-4 lg:px-10 gap-10 lg:gap-x-16 text-left relative"
+		class="container mx-auto flex flex-col lg:grid lg:grid-cols-4 items-center mt-12 px-4 lg:px-10 gap-10 lg:gap-x-16 text-left relative"
 	>
-		<div class="col-span-2 flex items-start self-start"><BackButton /></div>
+		<div class="lg:col-span-4 flex items-start self-start"><BackButton /></div>
 		{#await searchCountires(searchString, slug)}
-			<LoaderInline />
+			<div class="lg:col-span-4 flex items-start self-start"><LoaderCog /></div>
+		{:then countries}
+			{#each countries as country}
+				<CardSmall {country} />
+			{/each}
 		{:catch error}
 			<p>{error}</p>
 		{/await}
