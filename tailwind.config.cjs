b2a1c031d3b62d300d835469b1ea6f3903a9d3fb
Noxyangel <forgotrrr@gmail.com>
2022-12-26 23:06:45 +0300

    removed some shadows 0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -35,15 +35,11 @@ module.exports = {
 				'any-white': 'hsl(0, 0%, 100%)'
 			},
 			boxShadow: {
-				'img-light':
-					'0 0 7.5px 3px hsla(200, 15%, 8%, 0.549),0 0 11px 5px hsla(0, 0%, 52%, 0.362),0 0 17px 9px hsla(0, 0%, 98%, 0.18)',
-				'img-dark':
-					'0 0 7.5px 3px hsla(209, 23%, 22%, 0.549),0 0 11px 5px hsla(0, 0%, 100%, 0.362),0 0 17px 9px hsla(0, 0%, 100%, 0.18)',
-				'card-light':
-					'0 0 7.5px 3px hsla(200, 15%, 8%, 0.549),0 0 11px 5px hsla(0, 0%, 52%, 0.362),0 0 17px 9px hsla(0, 0%, 98%, 0.18),inset 0 0 2px 2px hsla(200, 15%, 8%, 0.549),inset 0 0 5px 4px hsla(0, 0%, 52%, 0.362),inset 0 0 9px 5px hsla(0, 0%, 98%, 0.18)',
-				'card-dark':
-					'0 0 3px 2px hsla(209, 23%, 22%, 0.549),0 0 6px 3px hsla(0, 0%, 100%, 0.362),0 0 10px 5px hsla(0, 0%, 100%, 0.18),inset 0 0 2px 1px hsla(209, 23%, 22%, 0.549),inset 0 0 5px 4px hsla(0, 0%, 100%, 0.362),inset 0 0 9px 5px hsla(0, 0%, 100%, 0.18)',
-				around: '0 0 5px 2px hsla(209, 23%, 22%, 0.549)'
+				'img-light': '0 0 7px 5px hsla(200, 15%, 8%, 0.362)',
+				'img-dark': '0 0 7px 5px hsla(0, 0%, 100%, 0.362)',
+				'card-light': '0 0 5px 5px hsla(200, 15%, 8%, 0.362)',
+				'card-dark': '0 0 5px 5px hsla(0, 0%, 100%, 0.362)',
+				around: '0 0 5px 2px hsla(209, 23%, 22%,  0.362)'
 			},
 			transitionTimingFunction: {
 				theme: 'cubic-bezier(0.005, 0.05, 0.95, 0.005)',
