9a40343cab6b2e8fdf5cfc0eb9e6cdcd37589ebf
Noxyangel <forgotrrr@gmail.com>
2022-12-27 09:29:45 +0300

    card flag aspect changed to 13/8

--- src/lib/CardSmall.svelte
+++ src/lib/CardSmall.svelte
@@ -21,7 +21,7 @@
 				alt={'' + country?.name?.common + ' flag'}
 				class="mx-auto  transition-colors duration-700 border-b-[1px]
 			 border-b-light-mode-dark-gray border-opacity-20 dark:border-opacity-20 dark:border-b-any-white 
-			 object-top object-fit w-full h-auto max-h-[12em] aspect-[2/1]"
+			 object-top object-fit w-full h-auto max-h-[12em] aspect-[13/8]"
 			/>
 		</div>
 		<!-- Info -->
